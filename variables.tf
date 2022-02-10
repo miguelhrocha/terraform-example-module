49187231c535774cac1ce8b9ed1fe39405f791d3
Miguel Hernández <miguel.hernandez@hashicorp.com>
2022-02-10 15:56:00 +0100

    add max pods per node variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "example" {
   description = "Example variable"
   default     = "hello world"
 }
+
+variable "default_max_pods_per_node" {
+  description = "The maximum number of pods to schedule per node"
+  default     = 110
+}
